0553ce39065df9e3e765e348f3d6cb6f84fba95a
Brian Marquis <rbmarquis.dev@gmail.com>
2022-07-28 00:25:01 -0500

    feat: toggle search box

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -5,6 +5,13 @@ import { connect } from "react-redux";
 import { UPDATE_TITLE_SEARCH_TERM } from "../../constants/actionTypes";
 
 const Banner = (props) => {
+  const [isSearchHidden, setIsSearchHidden] = React.useState(true);
+
+  const onClickHandler = (event) => {
+    event.preventDefault();
+    setIsSearchHidden(!isSearchHidden);
+  };
+
   const onChangeHandler = (event) => {
     event.preventDefault();
     const { value } = event.target;
@@ -19,21 +26,29 @@ const Banner = (props) => {
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div className="d-flex flex-row justify-content-center align-items-baseline">
-          <span id="get-part" className="">
+          <span
+            id="get-part"
+            style={{ cursor: "pointer" }}
+            onClick={onClickHandler}
+          >
             A place to get
           </span>
-          <form className="col-7">
-            <fieldset className="form-group">
-              <input
-                className="form-control"
-                id="search-box"
-                type="search"
-                placeholder="What is it that you truly desire?"
-                onChange={onChangeHandler}
-              />
-            </fieldset>
-          </form>
-          <span className=""> the cool stuff.</span>
+          {isSearchHidden ? (
+            <span>&nbsp;</span>
+          ) : (
+            <form className="col-7">
+              <fieldset className="form-group">
+                <input
+                  className="form-control"
+                  id="search-box"
+                  type="search"
+                  placeholder="What is it that you truly desire?"
+                  onChange={onChangeHandler}
+                />
+              </fieldset>
+            </form>
+          )}
+          <span>the cool stuff.</span>
         </div>
       </div>
     </div>
